5f4e6fb7235d7e3f6ead98fa4fd689b752d62bc9
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-11 11:54:41 +0100

    main controller created

---
+++ controllers/controller.pm
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+use Mojolicious::Lite -signatures;
+
+
+package controller;
+
+sub api ( $c, $name, $pages ) {
+   
+}
+
+return 1;
--- index.pl
+++ index.pl
@@ -1,6 +1,15 @@
+BEGIN {
+  unshift @INC,'C:\Users\Sergio\Desktop\NYC-SCRAPER';
+  #@INC is the directory list, where perl searches for .pm files
+}
+
+
 use Mojolicious::Lite -signatures;
+require controllers::controller;
 
 get "/:pages" => { pages => 1 } => sub ($c) {
+    my $pages = $c->param('pages');
+    controller::api( $c, "Sebastien", $pages );
     # $c -> render(text => "Hello !");
 };
 
