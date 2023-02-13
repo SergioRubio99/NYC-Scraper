db9a52ddf18a193d3626d95197011bf4fe362970
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-13 23:29:05 +0100

    Put getArticle function into the crawler module itself

--- functions/crawler.pm
+++ functions/crawler.pm
@@ -7,8 +7,8 @@ package crawler;
 use Mojolicious::Lite;
 use Mojo::DOM;
 use Mojo::Base -base;
-use functions::getArticle;
 use functions::getDOM;
+use functions::getUser;
 
 
 sub crawler {
@@ -18,7 +18,47 @@ sub crawler {
 
     my $DOM = getDOM::getDOM();
  
-    my @upperArr = getArticle::getArticle($DOM);
+
+     my $count    = 0;
+
+    # say "NUMBER, LENGTH OF THE ARTS ARRAY: ";
+    # say my $lowerArrScalar = @lowerArr;
+    my @upperArr = ();
+
+    my $lol = 0;
+
+    for my $e ( $DOM->at("#pagespace")->next_node->at("td")->at("table")
+        ->find(".athing")->each )
+    {
+        $count = $count + 1;
+        my $title = $e->at(".titleline")->at("a")->text;
+        my $link  = $e->at(".titleline")->at("a")->attr('href');
+        my $user  = getUser::getUser($e);
+        my $age   = $e->next_node->at(".age")->at("a")->text;
+        # my $comments =
+        #   $e->next_node->at(".age")
+        #   ->next_node->next_node->next_node->next_node->next_node->next_node
+        #   ->text;
+        # my $score   = $e->next_node->at(".score")->text;
+        my @artHash = (
+            "title"    => $title,
+            "user"     => $user,
+            "url"      => $link,
+            "age"      => $age,
+            "comments" => 1,
+            "score"    => 1
+        );
+
+        foreach my $entry ( values @artHash ) {
+
+            # say "";
+            # say $entry;
+            # say "";
+
+        }
+        push( @upperArr, {@artHash} );
+    }
+
     return @upperArr;
 }
 
