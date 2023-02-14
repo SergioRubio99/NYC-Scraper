01bd6a200b28eaac940121ea13960f4ead8a43b2
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-14 23:30:35 +0100

    getScore function now can take 0 points

--- functions/getScore.pm
+++ functions/getScore.pm
@@ -4,9 +4,12 @@ use Mojo::DOM;
 
 
 sub getScore ($html_el) {
-    my $score = $html_el -> next_node -> at(".score") -> text;
-    $score =~ s/\spoints//;
-    return $score+0;
+    if($html_el -> next_node -> at(".score")){
+        my $score = $html_el -> next_node -> at(".score") -> text;
+        $score =~ s/\spoints//;
+        return $score+0;
+    }
+    return 0;
 }
 
 
