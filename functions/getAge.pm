3e4bcd0ee7958d9da529dc1a57763d8000e531a2
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-14 23:21:36 +0100

    score points converted to a number value

--- functions/getAge.pm
+++ functions/getAge.pm
@@ -6,6 +6,7 @@ use Mojo::DOM;
 sub getAge ($html_el) {
     my $age = $html_el->next_node->at(".age")->at("a")->text;
     if ($age){
+        $age =~ s/\sago//;
         return $age;
     }
         return "";
--- functions/getScore.pm
+++ functions/getScore.pm
@@ -5,7 +5,8 @@ use Mojo::DOM;
 
 sub getScore ($html_el) {
     my $score = $html_el -> next_node -> at(".score") -> text;
-   return $score;
+    $score =~ s/\spoints//;
+    return $score+0;
 }
 
 
