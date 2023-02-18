3821561ba87431c06645a8e2a514d5ce460f9435
SergioRubio99 <legacyentertainmentyt@gmail.com>
2023-02-18 21:40:52 +0100

    Response array flattened and modified, to match the required specs

--- controllers/controller.pm
+++ controllers/controller.pm
@@ -10,19 +10,24 @@ use controllers::controller_methods::getPage;
 package controller;
 
 sub api ( $c, $name, $pages ) {
-    $_ = $pages;
-    $_ = 1 unless m/^[1-4]$/;
 
   my @crawled;
 
-  for(my $i = 1; $i <= $pages; $i++){
-     push(@crawled, getPage::getPage($i));
+  for (my $i = 1; $i <= $pages; $i++){
+  
+  my @pageArr = getPage::getPage($i);
+
+   for my $aref (@pageArr){
+      shift @$aref;
+   }
+
+   push(@crawled, @pageArr);
+
   }
-    # say @crawled;
+  #We flatten the array and send it:
     my $ArrRef = \@crawled;
-    # say $_;
-    # say "Hey! I'm on the top of the application!";
-    $c->render( json => $ArrRef );
+    @crawled = map {@$_} @$ArrRef;
+    $c->render(json => $ArrRef);
 }
 
 return 1;
